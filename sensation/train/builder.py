557d76316ba57db0e5e220320858426ca7607e5a
theanindya <40605723+theanindya@users.noreply.github.com>
2024-11-23 16:23:22 +0100

    Update builder.py

--- sensation/train/builder.py
+++ sensation/train/builder.py
@@ -1,3 +1,4 @@
+# This version of builder.py is specific to the SENSATION dataset. To use cityscape dataset, run the 2nd phase of the code only
 import logging
 import os
 from enum import Enum, auto
@@ -328,3 +329,308 @@ def load_checkpoint(model, ckpt_path, exclude_last=True):
 
 def create_segmentation_head(model, model_name, num_classes):
     pass
+
+# This version of builder.py is specific to the Cityscapes dataset.
+# Ensure that data loaders, loss functions, and transformations are correctly set up for Cityscapes.
+
+import logging
+import os
+from enum import Enum, auto
+import sys
+import segmentation_models_pytorch as smp
+import torch
+from torch.utils.data import DataLoader
+
+from sensation.models.segmentation import (
+    DeepLabv3Plus,
+    PSPNet,
+    SegModel,
+    UNet,
+    UnetPlusPlus,
+)
+from sensation.train.data import Cityscapes, Mapillary, SensationDS
+from sensation.utils import city_utils, map_utils
+
+logger = logging.getLogger(__name__)
+
+class LossFunction(Enum):
+    JACCARD = auto()
+    DICE = auto()
+    TVERSKY = auto()
+    FOCAL = auto()
+    CROSS = auto()
+    KNIGHT = auto()
+    SOFTBCE = auto()
+    SOFTCROSS = auto()
+    MCC = auto()
+
+class LossMode(Enum):
+    BINARY = auto()
+    MULTICLASS = auto()
+    MULTILABEL = auto()
+
+def get_loss(
+    loss_func: LossFunction = LossFunction.DICE, mode: LossMode = LossMode.MULTICLASS
+):
+    loss_mode = None
+    loss = None
+
+    if mode == LossMode.BINARY:
+        loss_mode = smp.losses.constants.BINARY_MODE
+    elif mode == LossMode.MULTICLASS:
+        loss_mode = smp.losses.constants.MULTICLASS_MODE
+    elif mode == LossMode.MULTILABEL:
+        loss_mode = smp.losses.constants.MULTILABEL_MODE
+    else:
+        err_msg = f"Unsupported loss mode: {mode}"
+        raise ValueError(err_msg)
+
+    if loss_func == LossFunction.JACCARD:
+        loss = smp.losses.JaccardLoss(mode=loss_mode)
+    elif loss_func == LossFunction.DICE:
+        loss = smp.losses.DiceLoss(mode=loss_mode)
+    elif loss_func == LossFunction.TVERSKY:
+        """FP and FN is weighted by alpha and beta params. With alpha == beta == 0.5, this loss becomes equal DiceLoss."""
+        loss = smp.losses.TverskyLoss(mode=loss_mode, alpha=0.45, beta=0.4, gamma=1.0)
+    elif loss_func == LossFunction.CROSS:
+        frequencies = torch.tensor(
+            [
+                2732,
+                2149,
+                2678,
+                321,
+                1434,
+                1181,
+                2089,
+                689,
+                266,
+                1124,
+                525,
+                649,
+                375,
+                295,
+            ],
+            dtype=torch.float32,
+        )
+        max_frequency = frequencies.max()
+        weights = max_frequency / frequencies
+        weights /= weights.sum() * len(weights)  # Normalize weights to sum to 1
+        loss = torch.nn.CrossEntropyLoss(weight=weights)
+    elif loss_func == LossFunction.FOCAL:
+        loss = smp.losses.FocalLoss(
+            mode=loss_mode,
+            alpha=None,
+            gamma=2.0,
+            ignore_index=None,
+            reduction="mean",
+            normalized=False,
+            reduced_threshold=None,
+        )
+    else:
+        err_msg = f"Not supported loss function: {loss_func}"
+        raise ValueError(err_msg)
+
+    return loss
+
+def create_seg_model(
+    model_arc: str = None,
+    epochs: int = 1,
+    num_classes: int = 8,
+    learning_rate: float = 1e-3,
+    batch_size: int = 1,
+    ckpt_path: str = "",
+    loss=None,
+    train_dataloader: DataLoader = None,
+    val_dataloader: DataLoader = None,
+    test_dataloader: DataLoader = None,
+):
+    logger.info("Starting to create segmentation model.")
+    base_model = None
+    model_name = None
+    encoder_name = None
+    model_output = None
+    parts = model_arc.split(":")
+    if len(parts) == 3:
+        model_name = parts[0].lower()
+        encoder_name = parts[1]
+        model_output = int(parts[2])
+        logger.debug(
+            f"Detected segmentation model: {model_name} with encode: {encoder_name}."
+        )
+    else:
+        error_msg = f"The model architecture is not correctly defined: {model_arc}"
+        raise ValueError(error_msg)
+
+    if model_name == "deeplabv3plus":
+        base_model = DeepLabv3Plus(num_classes=model_output, encoder_name=encoder_name)
+    elif model_name == "pspnet":
+        base_model = PSPNet(num_classes=model_output, encoder_name=encoder_name)
+    elif model_name == "unetpp":
+        base_model = UnetPlusPlus(num_classes=model_output, encoder_name=encoder_name)
+    elif model_name == "unet":
+        base_model = UNet(num_classes=model_output, encoder_name=encoder_name)
+    else:
+        error_msg = f"Unsupported model architecture: {model_arc}."
+        raise ValueError(error_msg)
+
+    model = SegModel(
+        num_classes=model_output,
+        learning_rate=learning_rate,
+        batch_size=batch_size,
+        epochs=epochs,
+        base_model=base_model,
+        loss=loss,
+        train_data=train_dataloader,
+        val_data=val_dataloader,
+        test_data=test_dataloader,
+    )
+
+    # Check if checkpoints exist then load
+    if ckpt_path and os.path.exists(ckpt_path):
+        model = load_checkpoint(model, ckpt_path, exclude_last=False)
+
+        # change last output layer if needed
+        if num_classes > model_output:
+            model.whole_model.arc.segmentation_head = create_segmentation_head(
+                model, model_name, num_classes
+            )
+            model.update_model(num_classes)
+
+    return model
+
+def create_dataloaders(train, val, test, batch_size: int):
+    train_loader = torch.utils.data.DataLoader(
+        dataset=train,
+        batch_size=batch_size,
+        num_workers=4,
+        shuffle=True,
+        persistent_workers=True,
+    )
+
+    val_loader = torch.utils.data.DataLoader(
+        dataset=val,
+        batch_size=batch_size,
+        num_workers=4,
+        shuffle=False,
+        persistent_workers=True,
+    )
+
+    test_loader = torch.utils.data.DataLoader(
+        dataset=test,
+        batch_size=batch_size,
+        num_workers=4,
+        shuffle=False,
+        persistent_workers=True,
+    )
+
+    return train_loader, val_loader, test_loader
+
+def prepare_cityscapes(
+    root_path: str,
+    batch_size: int,
+):
+    train_dataset = Cityscapes(
+        root_path,
+        split="train",
+        mode="fine",
+        target_type="semantic",
+        transform=city_utils.train_transform,
+        mask_transform=city_utils.convert_input_masks,
+    )
+
+    val_dataset = Cityscapes(
+        root_path,
+        split="val",
+        mode="fine",
+        target_type="semantic",
+        transform=city_utils.val_transform,
+        mask_transform=city_utils.convert_input_masks,
+    )
+
+    test_dataset = Cityscapes(
+        root_path,
+        split="test",
+        mode="fine",
+        target_type="semantic",
+        transform=city_utils.val_transform,
+        mask_transform=city_utils.convert_input_masks,
+    )
+
+    return create_dataloaders(train_dataset, val_dataset, test_dataset, batch_size)
+
+def prepare_mapillary(root_path: str, batch_size: int):
+    train_path = os.path.join(root_path, "training")
+    val_path = os.path.join(root_path, "validation")
+    test_path = os.path.join(root_path, "testing")
+    train_dataset = Mapillary(
+        train_path,
+        transform=map_utils.train_transform,
+        target_transform=map_utils.convert_input_masks,
+    )
+
+    val_dataset = Mapillary(
+        val_path,
+        transform=map_utils.val_transform,
+        target_transform=map_utils.convert_input_masks,
+    )
+
+    test_dataset = Mapillary(
+        test_path,
+        transform=map_utils.val_transform,
+        target_transform=map_utils.convert_input_masks,
+    )
+
+    return create_dataloaders(train_dataset, val_dataset, test_dataset, batch_size)
+
+def prepare_sensation(
+    root_dir: str,
+    batch_size: int,
+    image_height: int = 640,
+    image_width: int = 800,
+):
+    train_dataset = SensationDS(
+        root_dir=root_dir,
+        split="train",
+        image_height=image_height,
+        image_width=image_width,
+    )
+
+    val_dataset = SensationDS(
+        root_dir=root_dir,
+        split="val",
+        image_height=image_height,
+        image_width=image_width,
+    )
+
+    test_dataset = SensationDS(
+        root_dir=root_dir,
+        split="test",
+        image_height=image_height,
+        image_width=image_width,
+    )
+
+    return create_dataloaders(train_dataset, val_dataset, test_dataset, batch_size)
+
+def load_checkpoint(model, checkpoint_path, exclude_last=False):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    checkpoint = torch.load(checkpoint_path, map_location=device)
+    model.load_state_dict(checkpoint["state_dict"])
+
+    return model
+
+def create_segmentation_head(model, model_name, num_classes):
+    """Returns a specific segmentation head for a model."""
+    if model_name.startswith("pspnet"):
+        return smp.base.SegmentationHead(
+            in_channels=model.whole_model.arc.segmentation_head[0].in_channels,
+            out_channels=num_classes,
+            kernel_size=3,
+            upsampling=model.whole_model.arc.segmentation_head[1].scale_factor,
+        )
+    else:
+        return smp.base.SegmentationHead(
+            in_channels=model.whole_model.arc.segmentation_head[0].in_channels,
+            out_channels=num_classes,
+            kernel_size=3,
+        )
+
